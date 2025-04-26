c3d334bc9b493c55498b72bdf86741192913d686
Emilia <emiliasavich@gmail.com>
2025-04-26 16:00:03 -0400

    removed_unnecessary_files

--- dataPipeline/createSegment.py
+++
@@ -1,3 +0,0 @@
-def createSegment(transcript, timestamps, video_name):
-    segment = {'transcript': transcript, 'timestamps': timestamps, 'video_name': video_name}
-    return segment
--- dataPipeline/retrieveTranscripts.py
+++
@@ -1,9 +0,0 @@
-from mongoFunctions import MongoRetrieve
-
-def retrieveTranscripts():
-    transcripts = {}
-    data = MongoRetrieve()
-    for d in data:
-        transcripts[d['video_name']] = d['transcript']
-    return transcripts
-
